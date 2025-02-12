8a788701c503e0bbaadf34e4cee6c2b80c5ced28
mcriq <125591030+mcriq@users.noreply.github.com>
2025-02-12 15:20:16 -0600

    create read func

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,33 @@
 package config
 
+import (
+	"os"
+	"path/filepath"
+	"encoding/json"
+)
+
 
 type Config struct {
 	DbURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+
+func Read() (Config, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(filepath.Join(homeDir, ".gatorconfig.json"))
+	if err != nil {
+		return Config{}, err
+	}
+	config := Config{}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
 }
