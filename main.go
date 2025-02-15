b998049e9c0ac7f3e3d0a85f35f00ec7a53a0540
mcriq <125591030+mcriq@users.noreply.github.com>
2025-02-15 10:29:49 -0600

    add run and register commands

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/mcriq/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
@@ -15,17 +18,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = cfg.SetUser("Matt")
-	if err != nil {
-		log.Fatalf("unable to set user: %v\n", err)
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command)error),
+	}
+
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: cli <command> [args...]")
 		os.Exit(1)
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("unable to read config file: %v\n", err)
-		os.Exit(1)
-	}
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
 
-	fmt.Println(cfg.DBURL)
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
